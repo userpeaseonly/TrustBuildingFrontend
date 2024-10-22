8eec743cc109bea1ee297289cdb366d61593cd12
userpeaseonly <userpeaseonly@gmail.com>
2024-10-22 15:53:03 +0500

    make payment for terminated contract is finished

--- src/components/TerminateContractDetailsCard.vue
+++ src/components/TerminateContractDetailsCard.vue
@@ -2,21 +2,21 @@
     <div class="card mb-4 shadow-lg border-0 rounded-lg">
         <div class="card-body p-4">
             <h5 class="card-title text-primary d-flex align-items-center mb-3">
-                <i class="bi bi-file-text-fill me-2"></i> Contract #{{ contract.id }}
+                <i class="bi bi-file-text-fill me-2"></i> Contract #{{ contract.contract.id }}
             </h5>
 
             <!-- Section for Contract Overview -->
             <div class="mb-4">
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-calendar-check-fill me-2"></i>
-                    <strong>Contract Date: &nbsp;</strong> {{ contract.contract_date }}
+                    <strong>Contract Date: &nbsp;</strong> {{ contract.contract.contract_date }}
                 </p>
 
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-check-circle-fill me-2"></i>
                     <strong>Status: &nbsp;</strong>
-                    <span :class="getStatusBadgeClass(contract.status)"
-                        class="px-3 py-1 rounded-full text-sm font-semibold uppercase tracking-wide">{{ contract.status
+                    <span :class="getStatusBadgeClass(contract.contract.status)"
+                        class="px-3 py-1 rounded-full text-sm font-semibold uppercase tracking-wide">{{ contract.contract.status
                         }}</span>
                 </p>
             </div>
@@ -27,37 +27,37 @@
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-building me-2"></i>
                     <strong>Apartment Number: &nbsp;</strong>
-                    {{ contract.apartment.apartment_number }}
+                    {{ contract.contract.apartment.apartment_number }}
                 </p>
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-stack me-2"></i>
                     <strong>Floor Number: &nbsp;</strong>
-                    {{ contract.apartment.floor_number }}
+                    {{ contract.contract.apartment.floor_number }}
                 </p>
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-door-open-fill me-2"></i>
                     <strong>Building Entrance: &nbsp;</strong>
-                    {{ contract.apartment.building_entrance }}
+                    {{ contract.contract.apartment.building_entrance }}
                 </p>
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-fullscreen-exit me-2"></i>
                     <strong>Total Area: &nbsp;</strong>
-                    {{ contract.apartment.total_area }} m²
+                    {{ contract.contract.apartment.total_area }} m²
                 </p>
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-aspect-ratio-fill me-2"></i>
                     <strong>Living Area: &nbsp;</strong>
-                    {{ contract.apartment.living_area }} m²
+                    {{ contract.contract.apartment.living_area }} m²
                 </p>
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-calendar-fill me-2"></i>
                     <strong>Created At: &nbsp;</strong>
-                    {{ formatDate(contract.apartment.created_at) }}
+                    {{ formatDate(contract.contract.apartment.created_at) }}
                 </p>
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-calendar-check-fill me-2"></i>
                     <strong>Updated At: &nbsp;</strong>
-                    {{ formatDate(contract.apartment.updated_at) }}
+                    {{ formatDate(contract.contract.apartment.updated_at) }}
                 </p>
             </div>
 
@@ -67,23 +67,23 @@
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-person-fill me-2"></i>
                     <strong>Customer: &nbsp;</strong>
-                    {{ contract.customer.first_name }} {{ contract.customer.last_name }} {{ contract.customer.third_name
+                    {{ contract.contract.customer.first_name }} {{ contract.contract.customer.last_name }} {{ contract.contract.customer.third_name
                     }}
                 </p>
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-telephone-fill me-2"></i>
                     <strong>Phone Number: &nbsp;</strong>
-                    {{ contract.customer.user.phone_number }}
+                    {{ contract.contract.customer.user.phone_number }}
                 </p>
-                <p v-if="contract.customer.phone_number2" class="card-text d-flex align-items-center">
+                <p v-if="contract.contract.customer.phone_number2" class="card-text d-flex align-items-center">
                     <i class="bi bi-telephone-fill me-2"></i>
                     <strong>Phone Number 2: &nbsp;</strong>
-                    {{ contract.customer.phone_number2 }}
+                    {{ contract.contract.customer.phone_number2 }}
                 </p>
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-file-earmark-text-fill me-2"></i>
                     <strong>Passport Issuance Place: &nbsp;</strong>
-                    {{ contract.customer.passport_place_issuance }}
+                    {{ contract.contract.customer.passport_place_issuance }}
                 </p>
             </div>
 
@@ -93,27 +93,61 @@
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-cash-stack me-2"></i>
                     <strong>Price per Square: &nbsp;</strong>
-                    {{ formatPrice(contract.price_per_square) }} UZS
+                    {{ formatPrice(contract.contract.price_per_square) }} UZS
                 </p>
 
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-currency-exchange me-2"></i>
                     <strong>Contract Amount: &nbsp;</strong>
-                    {{ formatPrice(contract.contract_amount) }} UZS
+                    {{ formatPrice(contract.contract.contract_amount) }} UZS
                 </p>
 
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-clock-history me-2"></i>
                     <strong>Payment Months: &nbsp;</strong>
-                    {{ contract.payment_months }}
+                    {{ contract.contract.payment_months }}
                 </p>
 
                 <p class="card-text d-flex align-items-center">
                     <i class="bi bi-wallet-fill me-2"></i>
                     <strong>Monthly Payment: &nbsp;</strong>
-                    {{ formatPrice(contract.monthly_payment) }} UZS
+                    {{ formatPrice(contract.contract.monthly_payment) }} UZS
                 </p>
             </div>
+
+            <!-- Section for Termination Details -->
+            <div class="border-top pt-3 mt-3">
+                <h6 class="text-muted mb-3">Termination Details</h6>
+                <p class="card-text d-flex align-items-center">
+                    <i class="bi bi-cash-stack me-2"></i>
+                    <strong>Termination fine: &nbsp;</strong>
+                    {{ formatPrice(contract.termination_fine_amount) }} UZS
+                </p>
+
+                <p class="card-text d-flex align-items-center">
+                    <i class="bi bi-currency-exchange me-2"></i>
+                    <strong>Termination Planned Payment: &nbsp;</strong>
+                    {{ formatPrice(contract.termination_planned_payment) }} UZS
+                </p>
+
+                <p class="card-text d-flex align-items-center">
+                    <i class="bi bi-clock-history me-2"></i>
+                    <strong>Termination Reason: &nbsp;</strong>
+                    {{ contract.termination_reason }}
+                </p>
+
+                <p class="card-text d-flex align-items-center">
+                    <i class="bi bi-wallet-fill me-2"></i>
+                    <strong>Termination Date: &nbsp;</strong>
+                    {{ contract.termination_date }}
+                </p>
+                <p class="mt-4">
+                    <i class="bi bi-wallet-fill me-2"></i>
+                    <strong class="text-warning">Paid Amount: &nbsp;</strong>
+                    <span class="border py-2 px-3 border-warning border-2 rounded">{{ formatPrice(contract.termination_payment_paid_amount) }} UZS</span>
+                </p>
+            </div>
+
         </div>
     </div>
 
